b70937de39d210cdeae795a41a63af94ce841148
Ilya Bagay <ilyabaglay20@gmail.com>
2024-10-22 23:04:51 +0900

    index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,21 @@
-import { NextPageAuth } from "@/providers/auth-providers/auth-page.types";
 import Home from "@/screens/home/Home";
 import { ProductService } from "@/services/product/product.sevice";
-import { IProduct, TypePaginationProducts } from "@/types/product.interface";
-import { GetStaticProps, NextPage } from "next";
+import { TypePaginationProducts } from "@/types/product.interface";
+import { GetServerSideProps, NextPage } from "next";
 
+const HomePage: NextPage<TypePaginationProducts> = ({ lenght, products }) => {
+  return <Home products={products} lenght={length} />;
+};
 
-const HomePage: NextPage<TypePaginationProducts> = ({lenght, products}) => {
-  return <Home products={products} lenght={lenght}/>
-}
-
-export const getStaticProps: GetStaticProps<TypePaginationProducts> = async () => {
+export const getServerSideProps: GetServerSideProps<TypePaginationProducts> = async () => {
   const data = await ProductService.getAll({
     page: 1,
     perPage: 100000
-  })
+  });
 
   return {
     props: data
-  }
-}
+  };
+};
 
-export default HomePage
+export default HomePage;
